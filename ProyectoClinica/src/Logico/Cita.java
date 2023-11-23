2b89768f821c9ea8b42e503c6b7657a8b33d4f6b
Erick de la rosa <133063517+Estudiante2810@users.noreply.github.com>
2023-11-23 14:58:47 -0400

    Creacion de las clases: Paciente, Enfermedad y Cita. Puede que se necesite cambiar cita en un futuro

---
+++ ProyectoClinica/src/Logico/Cita.java
@@ -0,0 +1,13 @@
+package Logico;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Cita {
+	
+	private Date fechaCita;
+	private ArrayList<Persona> persona;
+	private ArrayList<Doctor> doctor;
+	private ArrayList<Clinica> cliente; 
+
+}
---
+++ ProyectoClinica/src/Logico/Enfermedad.java
@@ -0,0 +1,27 @@
+package Logico;
+
+public class Enfermedad {
+	
+	private String Nombre;
+	private String Descripcion;
+	
+	
+	public Enfermedad(String nombre, String descripcion) {
+		super();
+		Nombre = nombre;
+		Descripcion = descripcion;
+	}
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+}
