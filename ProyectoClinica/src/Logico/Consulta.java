6ed743d743907adf9872d572b65ca118152e06ec
GOC0 <133062374+GOC0@users.noreply.github.com>
2023-11-23 15:15:32 -0400

    aggregacion

--- ProyectoClinica/src/Logico/Consulta.java
+++ ProyectoClinica/src/Logico/Consulta.java
@@ -2,19 +2,26 @@ package Logico;
 
 public class Consulta {
 	
+	private String NombreDoc;
 	private Paciente paciente;
 	private Arraylist<Enfermedad> enfermedad;
 	private HistorialClinico histoClinico;
 	private Date fecha;
-	public Consulta(Paciente paciente, Arraylist<Enfermedad> enfermedad, HistorialClinico histoClinico, Date fecha) {
+	public Consulta(String nombreDoc, Paciente paciente, Arraylist<Enfermedad> enfermedad,
+			HistorialClinico histoClinico, Date fecha) {
 		super();
+		NombreDoc = nombreDoc;
 		this.paciente = paciente;
 		this.enfermedad = enfermedad;
 		this.histoClinico = histoClinico;
 		this.fecha = fecha;
 	}
-	
-	
+	public String getNombreDoc() {
+		return NombreDoc;
+	}
+	public void setNombreDoc(String nombreDoc) {
+		NombreDoc = nombreDoc;
+	}
 	public Paciente getPaciente() {
 		return paciente;
 	}
@@ -39,5 +46,7 @@ public class Consulta {
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
+	
+	
 	
 }
