17bfb48fe1b733964203d9c1526e63cf74665b82
GOC0 <133062374+GOC0@users.noreply.github.com>
2023-11-26 17:15:19 -0400

    creacion (visual) registrar persona -RegiPerso

--- ProyectoClinica/src/Visual/RegiPerso.java
+++ ProyectoClinica/src/Visual/RegiPerso.java
@@ -7,6 +7,8 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.JRadioButton;
 
 public class RegiPerso extends JDialog {
 
@@ -29,11 +31,36 @@ public class RegiPerso extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegiPerso() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 344);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "informacion", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(10, 11, 414, 122);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(10, 144, 414, 33);
+		contentPanel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("paciente");
+		rdbtnNewRadioButton.setBounds(16, 7, 109, 23);
+		panel_1.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("doctor");
+		rdbtnNewRadioButton_1.setBounds(233, 7, 109, 23);
+		panel_1.add(rdbtnNewRadioButton_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new TitledBorder(null, "Paciente", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_2.setBounds(10, 188, 414, 73);
+		contentPanel.add(panel_2);
+		panel_2.setLayout(null);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -51,5 +78,4 @@ public class RegiPerso extends JDialog {
 			}
 		}
 	}
-
 }
