8d6ee371ea3847868ccc16574b40a10bb2ba723c
GOC0 <133062374+GOC0@users.noreply.github.com>
2023-12-06 11:27:06 -0400

    listar vacunas

---
+++ ProyectoClinica/src/Visual/ListadoVacuna.java
@@ -0,0 +1,104 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import com.sun.glass.events.MouseEvent;
+
+import Logico.Clinica;
+import Logico.Paciente;
+import Logico.Persona;
+import Logico.Vacuna;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+
+public class ListadoVacuna extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+	private JButton okButton;
+	private JButton cancelButton;
+	private static DefaultTableModel model;
+	private Object rowData[];
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ListadoVacuna dialog = new ListadoVacuna();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ListadoVacuna() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 414, 217);
+		contentPanel.add(scrollPane);
+		{
+			String[] header = {"Código", "Nombre", "Trata"};
+        model = new DefaultTableModel();
+        model.setColumnIdentifiers(header);
+        table = new JTable();
+       
+        table.setModel(model);
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        table.setModel(model);
+        scrollPane.setViewportView(table);
+		}
+		
+		
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+		loadVacuna();
+	}
+	private void loadVacuna() {
+		model.setRowCount(0);
+
+		for (Vacuna vacun : Clinica.getInstance().getMisVacunas()) {
+			
+				rowData = new Object[model.getColumnCount()];
+
+				rowData[0] = vacun.getCodigo();
+				rowData[1] = vacun.getNombre();
+				rowData[2] = vacun.getEnfermedad().getNombre();
+
+				model.addRow(rowData);
+		}
+	}
+}
